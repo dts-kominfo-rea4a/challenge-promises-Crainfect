76bb1d4fd9352460fe60349230b12205e6170d46
Crainfect <95446332+Crainfect@users.noreply.github.com>
2022-09-19 23:56:02 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,4 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const fs=require("fs/promises")
+
+promiseTheaterIXX()
+  .then((datahasilresolve)=>{console.log(datahasilresolve)})
+//console.log(datahasilresolve)
+promiseTheaterVGC()
+  .then((datavgc)=>{console.log(datavgc)})
+
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = null;
@@ -6,3 +14,4 @@ const promiseOutput = null;
 module.exports = {
   promiseOutput,
 };
+console.log()
